7b4392bc5ddc24aaefa1df5acc4ea7457467e2ab
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-28 09:16:29 -1000

    created listview layout

--- app/src/main/java/com/myappcompany/russell/newsreader/MainActivity.java
+++ app/src/main/java/com/myappcompany/russell/newsreader/MainActivity.java
@@ -2,12 +2,26 @@ package com.myappcompany.russell.newsreader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> titles = new ArrayList<>();
+
+    ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.listView);
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, titles);
+
+        listView.setAdapter(arrayAdapter);
     }
 }
