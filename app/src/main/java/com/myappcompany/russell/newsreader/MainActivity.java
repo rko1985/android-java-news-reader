293b762b3e5523a7d252b0de9e5c675e257cd342
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-28 10:41:40 -1000

    get list of top news story id's from hacker news api

--- app/src/main/java/com/myappcompany/russell/newsreader/MainActivity.java
+++ app/src/main/java/com/myappcompany/russell/newsreader/MainActivity.java
@@ -1,10 +1,16 @@
 package com.myappcompany.russell.newsreader;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,10 +24,50 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        DownloadTask task = new DownloadTask();
+        try {
+            task.execute("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
+        } catch (Exception e) {
+
+        }
+
         ListView listView = findViewById(R.id.listView);
 
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, titles);
 
         listView.setAdapter(arrayAdapter);
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                int data = inputStreamReader.read();
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = inputStreamReader.read();
+                }
+
+                Log.i("URL Content", result);
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 }
